7abb60c9ea1c3a4f9d15555b229160a7ce8d0d2d
shivanandagouda <50527189+shivanandagouda@users.noreply.github.com>
2019-05-24 16:21:10 +0530

    New

---
+++ new.java
@@ -0,0 +1,9 @@
+public class Hello{
+pulbic static void main (String[] args){     
+            for(int i=1;i<=10;i++){
+
+ system.out.println("Hello world ..."+i);
+               
+                  }
+            }  
+}
